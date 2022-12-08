8251d48292c5e09a5dac1129fdf596785819eb94
Joseph Burke <jjburke011@gmail.com>
2022-12-07 19:06:14 -0600

    fixed cursor issue somehow

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -52,9 +52,10 @@ func _process(delta):
 	hitbox.position = aiming * 16
 	direction_cursor.position = aiming * 16
 	if ((get_global_mouse_position() - self.position).length() <= 16):
-		direction_cursor.look_at(-1* aiming)
+		direction_cursor.flip_h = true
 	else :
-		direction_cursor.look_at(get_global_mouse_position())
+		direction_cursor.flip_h = false
+	direction_cursor.look_at(get_global_mouse_position())
 	hitbox.look_at(get_global_mouse_position())
 	select_animation()
 	if(!is_flying):
